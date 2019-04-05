e8f955f0916dc2de449b3e1d3876242822d8e43b
Bryant Luu <bryant@numer.ai>
2019-04-04 19:10:57 -0700

    wip tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Input, MenuItem } from "@material-ui/core";
+import axios from "axios";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    suggestions: [],
+    inputValue: ""
+  };
+
+  handleChange = event => {
+    const inputValue = event.target.value;
+    this.setState({ inputValue });
+
+    if (!inputValue) {
+      this.setState({ suggestions: [] });
+    } else {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/tv?api_key=e1c030c90262b41deac61b0565e22368&language=en-US&page=1&query=${inputValue}`
+        )
+        .then(res => {
+          const suggestions = res.data.results.map(movie => movie.name);
+          this.setState({ suggestions });
+        });
+    }
+  };
+
+  handleSuggestionClick = value => {
+    this.setState({ suggestions: [], inputValue: value });
+  };
+
+  renderSuggestions() {
+    return this.state.suggestions.map((suggestion, key) => {
+      return (
+        <MenuItem
+          key={key}
+          component="div"
+          onClick={() => this.handleSuggestionClick(suggestion)}
+        >
+          {suggestion}
+        </MenuItem>
+      );
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-suggestion-container">
+          <Input
+            onChange={this.handleChange}
+            value={this.state.inputValue}
+            fullWidth
+          />
+          {this.renderSuggestions()}
+        </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Input, MenuItem } from "@material-ui/core";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("sums numbers", () => {
+  App.state = {
+    suggestions: ["foo", "foo"]
+  };
+
+  console.log(App.renderSuggestions());
+  expect(1).toEqual(2);
 });
